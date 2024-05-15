f2ded7105a8d69c4718e369a367f3cd1ddeeac9a
vbounmy <valentine.bounmy@gmail.com>
2024-05-15 16:56:32 +0200

    :boom: add pokemon font + section pokemon

---
+++ src/PokeApi.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class PokeApi extends Component {
+  state = {
+    data: [],
+  };
+  componentDidMount() {
+    let url = "https://pokeapi.co/api/v2/pokemon/snorlax";
+    fetch(url)
+      .then((result) => result.json())
+      .then((result) => {
+        this.setState({
+          data: result,
+        });
+      });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div class="pokemon">
+        <h1>Pokemon</h1>
+        <img src={data.sprites?.front_default}></img>
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Height</th>
+            <th>Weight</th>
+            <th>Types</th>
+          </thead>
+          <td>{data.id}</td>
+          <td>{data.name}</td>
+          <td>{data.height}</td>
+          <td>{data.weight}</td>
+          <td>
+            {data.types &&
+              data.types.map((typeInfo, index) => (
+                <span key={index}>
+                  {typeInfo.type.name}
+                  {index < data.types.length - 1 ? ", " : ""}
+                </span>
+              ))}
+          </td>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default PokeApi;
--- src/Qr.js
+++ src/Qr.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import Q from "./Q.js";
 import R from "./R.js";
+import PokeApi from "./PokeApi.js";
 
 class Qr extends Component {
   state = {
     dataToPass: [
       { numero: "0001", nom: "Bulbizarre", type: "Plante, Poison" },
-      // { numero: "0002", nom: "Herbizarre", type: "Plante, Poison" },
-      // { numero: "0003", nom: "Florizarre", type: "Plante, Poison" },
+      { numero: "0002", nom: "Herbizarre", type: "Plante, Poison" },
+      { numero: "0003", nom: "Florizarre", type: "Plante, Poison" },
       { numero: "0004", nom: "Salamèche", type: "Feu" },
-      // { numero: "0005", nom: "Reptincel", type: "Feu" },
-      // { numero: "0006", nom: "Dracaufeu", type: "Feu, Vol" },
+      { numero: "0005", nom: "Reptincel", type: "Feu" },
+      { numero: "0006", nom: "Dracaufeu", type: "Feu, Vol" },
       { numero: "0007", nom: "Carapuce", type: "Eau" },
-      // { numero: "0008", nom: "Carabaffe", type: "Eau" },
-      // { numero: "0009", nom: "Tortank", type: "Eau" },
+      { numero: "0008", nom: "Carabaffe", type: "Eau" },
+      { numero: "0009", nom: "Tortank", type: "Eau" },
     ],
   };
 
@@ -41,11 +42,17 @@ class Qr extends Component {
           removeRow={this.removeRow}
         />
         <h1></h1>
-        <Q
+        {/* <Q
           tableName={tableTitle2}
           tableContent={dataToPass}
           removeRow={this.removeRow}
-        />
+        /> */}
+
+        <h1></h1>
+
+        <PokeApi />
+
+        <h1></h1>
 
         <R />
       </div>
